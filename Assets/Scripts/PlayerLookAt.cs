27427354422483995f10076d016c3beb9d3b3fc6
Georgegipa <george.gipa@yahoo.gr>
2022-04-20 03:21:25 +0300

    Collision based info panel generation
    Added the ability to change the info panel when a player intersects with a collider. The info panel gets disabled once the player leaves a collision area!
    Demo scene name: LookingTest

--- Assets/Scripts/PlayerLookAt.cs
+++ Assets/Scripts/PlayerLookAt.cs
@@ -1,50 +1,100 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
+using Newtonsoft.Json;
 
 public class PlayerLookAt : MonoBehaviour
 {
-    //Warning spaghetti! Special equipment required. 
-    //You have been warned
-    [SerializeField] Canvas _canvas;
-    [SerializeField] TextAsset jsonFile;
+    //Warning spaghetti! You have been warned!
+    [SerializeField] Canvas _canvas; //the canvas that will be used to display info and the cart
+    [SerializeField] TextAsset jsonFile;//the json file that will be used to retrieve the data for every exhibit
     private GameObject infoPanel, infoPicture, infoText;
+    private Dictionary<string, Exhibit> exhibitDictionary;
 
+    //The following objects are used to store the information from the json file
+    [Serializable]
     public class Exhibit
     {
         public string path;
         public string info;
-        public int qunatity;
+        public int quantity;
         public float price;
     }
 
-    [System.Serializable]
-    public class Exhibits
+    //This can be done better using unity's built in json parser but I can't get it to work
+    void JSONToDictionary()
     {
-        public Exhibit[] exhibit;
+        //deserialize the json file
+        DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(jsonFile.text);
+        DataTable dataTable = dataSet.Tables["exhibits"];
+        //create a dictionary to store the data
+        exhibitDictionary = new Dictionary<string, Exhibit>();
+        //loop through the data and add it to the dictionary
+        foreach (DataRow row in dataTable.Rows)
+        {
+            Exhibit exhibit = new Exhibit();
+            exhibit.path = row["path"].ToString();
+            exhibit.info = row["info"].ToString();
+            exhibit.quantity = int.Parse(row["quantity"].ToString());
+            exhibit.price = float.Parse(row["price"].ToString());
+            exhibitDictionary.Add(row["name"].ToString(), exhibit);
+        }
+
+        //now display the data
+        foreach (KeyValuePair<string, Exhibit> entry in exhibitDictionary)
+        {
+            Debug.Log(entry.Key + " - " + entry.Value.info);
+        }
+    }
+
+    //Place the info of the exhibit in the info panel
+    void JSONtoInfoPanel(string exhibit_tag = "Exhibit 1")
+    {
+        if (exhibitDictionary.ContainsKey(exhibit_tag))
+        {
+            infoPanel.SetActive(true);
+            infoText.GetComponent<Text>().text = exhibitDictionary[exhibit_tag].info;
+            infoPicture.GetComponent<Image>().sprite = Resources.Load<Sprite>(exhibitDictionary[exhibit_tag].path);
+        }
+    }
+
+    //Retrieve the individual objects from info panel
+    void InfoPanelObjects()
+    {
+        infoPanel = _canvas.GetComponent<Transform>().GetChild(0).gameObject;
+        var infopanelobjects = infoPanel.GetComponent<Transform>();
+        infoPicture = infopanelobjects.GetChild(0).gameObject;
+        infoText = infopanelobjects.GetChild(1).gameObject;
     }
 
     void Start()
     {
-        //Get the first gameobject(This gets the info panel)
-        var infoPanel = _canvas.GetComponent<Transform>().GetChild(0).gameObject;
-        var infopanelobjects = infoPanel.GetComponent<Transform>();
-        var infoPicture = infopanelobjects.GetChild(0).gameObject;
-        infoText = infopanelobjects.GetChild(1).gameObject;
-        infoText.GetComponent<Text>().text = "Hello World";
-        Debug.Log("Hello there");
+        InfoPanelObjects();
+        infoPanel.SetActive(false);
+        JSONToDictionary();
+    }
 
-        //get things from json
-        Exhibits list = new Exhibits();
-        list = JsonUtility.FromJson<Exhibits>(jsonFile.text);
-        foreach (var ex in list.exhibit)
+    //What happens when the player enter a exhibit's collider
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != null)
         {
-            Debug.Log(ex.path + ex.info + ex.qunatity + ex.price);
+            JSONtoInfoPanel(other.tag);
         }
     }
 
-    // Update is called once per frame
+    //What happens when the player leaves a exhibit's collider
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != null)
+        {
+            infoPanel.SetActive(false);
+        }
+    }
+
+    //placeholder , not needed for this script
     void Update()
     {
     }
