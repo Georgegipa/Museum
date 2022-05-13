ee6114dbed8a2c589a0e24765fad6d6fb61385c3
MargaKMus <margaritakoump@gmail.com>
2022-05-13 17:19:40 +0300

    Adding Agent Window

---
+++ Assets/Scripts/AgentTrigger.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentTrigger : MonoBehaviour
+{
+    //Warning spaghetti! You have been warned!
+    [SerializeField] Canvas _canvas; //the canvas that will be used to display info and the cart
+    private GameObject infoPanel;
+
+    //Place the info of the exhibit in the info panel
+    void CanvasToPanel(string exhibit_tag = "Agent")
+    {
+        infoPanel.SetActive(true);
+    }
+
+    //Retrieve the individual objects from info panel
+    void InfoPanelObjects()
+    {
+        infoPanel = _canvas.GetComponent<Transform>().gameObject;
+    }
+
+    void Start()
+    {
+        InfoPanelObjects();
+        infoPanel.SetActive(false);
+    }
+
+    //What happens when the player enter a exhibit's collider
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != null)
+        {
+            CanvasToPanel(other.tag);
+        }
+    }
+
+    //What happens when the player leaves a exhibit's collider
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != null)
+        {
+            infoPanel.SetActive(false);
+        }
+    }
+
+    //placeholder , not needed for this script
+    void Update()
+    {
+    }
+}
